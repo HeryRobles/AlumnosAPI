d4f8712732b9e5fad40433c5bffb279cb226418b
rrobl <rrobl@HERYROBLES>
2022-11-09 22:35:06 -0700

    Agregar archivos de proyecto.

---
+++ AlumnosAPI/Model/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using AlumnosAPI.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlumnosAPI.Model
+{
+    public class ApplicationDbContext : DbContext 
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) 
+        {
+
+        }
+        
+        public DbSet<Alumno> Alumnos { get; set; }
+
+        public DbSet<Materia> Materias { get; set; }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Alumno>().ToTable("Alumnos");
+        //    modelBuilder.Entity<Materia>().ToTable("Materias");
+        //}
+    }
+}
---
+++ AlumnosAPI/Model/Entities/Materia.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AlumnosAPI.Model.Entities
+{
+    public class Materia
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int MateriaId { get; set; }
+        public string Nombre { get; set; }
+
+        public int Id { get; set; }
+        [ForeignKey("Id")]
+
+        public virtual Alumno alumno { get; set; }
+    }
+}
